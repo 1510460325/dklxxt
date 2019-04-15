50cee60af0e16fc0c93dfc17532a3f029be2749a
1510460325@qq.com <1510460325@qq.com>
2019-04-15 12:50:21 +0800

    add the build

---
+++ src/main/webapp/build/precache-manifest.9f2d7208007617baac6e737315673176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "334a859b1c50a610f3f7c38503a34859",
+    "url": "/static/media/t.334a859b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a400daaac86fe51b1d4",
+    "url": "/static/js/main.9a400daa.chunk.js"
+  },
+  {
+    "revision": "12bd4578ab5029cf1dd0",
+    "url": "/static/js/1.12bd4578.chunk.js"
+  },
+  {
+    "revision": "9a400daaac86fe51b1d4",
+    "url": "/static/css/main.cc7ffa80.chunk.css"
+  },
+  {
+    "revision": "ab7d069c64d2e65805039bf393280bbc",
+    "url": "/index.html"
+  }
+];
